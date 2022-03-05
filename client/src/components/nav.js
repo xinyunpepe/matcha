0b7d486f05a09910d3a13195a1bf92fe002864cd
Pepe <xinyun.pepe@gmail.com>
2022-03-05 18:42:35 +0100

    start backend

---
+++ client/src/components/nav.js
@@ -0,0 +1,31 @@
+const Nav = ({ minimal, setShowModal, showModal, setIsSignup}) => {
+
+	const handleClick = () => {
+		setShowModal(true);
+		setIsSignup(false);
+	}
+
+	const authToken = false;
+
+	return (
+		<nav>
+			<div className="logo-container">
+				{ minimal ? <p className='text-logo-green'>🍵matcha</p>
+				: <p className='text-logo-white'>🍵 matcha</p> }
+
+				{/* <img className="logo" src={ minimal ? textLogo : emojiLogo }/> */}
+
+			</div>
+			{/* if not logged in yet(!authToken), showes log in button */}
+			{ !authToken && !minimal && <button
+				className='nav-button'
+				// shows the modal if click login
+				onClick={ handleClick }
+				// disable the button once the modal shown
+				disabled={ showModal }
+			>Log in</button> }
+		</nav>
+	)
+}
+
+export default Nav
